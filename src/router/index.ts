80569279335dc90e5fd3c3073a17ca4d75dccdb8
youle <593370963@qq.com>
2025-07-23 11:20:58 +0800

    重构: 清理业务代码准备新业务开发

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
+
 // 配置路由信息
 const routes: RouteRecordRaw[] = [
   {
@@ -7,26 +8,10 @@ const routes: RouteRecordRaw[] = [
     redirect: '/main',
   },
   {
-    name: 'login',
-    path: '/login',
-    component: () => import('/@/views/login/index.vue'),
-  },
-  {
-    name: 'store',
-    path: '/store',
-    component: () => import('/@/views/store/index.vue'),
-  },
-  {
     name: 'main',
     path: '/main',
     component: () => import('/@/views/main/MainPage.vue'),
-    redirect: '/main/dashboard/workplace',
     children: [],
-  },
-  {
-    name: 'request',
-    path: '/request',
-    component: () => import('/@/views/request/index.vue'),
   },
 ]
 
--- src/views/main/components/MainHeader.vue
+++ src/views/main/components/MainHeader.vue
@@ -1,35 +1,26 @@
 <script lang="ts" setup>
-import { useUserStore } from '/@/store'
-const activeIndex = computed(() => {
-  return useRoute().path
-})
-const menuList = computed(() => {
-  return useUserStore().menuList
-})
+// 简单的头部组件，可以根据新业务需要进行扩展
 </script>
-<template>
-  <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo"
-    router
-    mode="horizontal"
-  >
-    <template v-for="menu in menuList" :key="menu.id">
-      <!-- 没有子路由的情况 -->
-      <el-menu-item v-if="!menu.children" :index="'/main' + menu.path">
-        <span>{{ menu.name }}</span>
-      </el-menu-item>
 
-      <!-- 存在子路由的情况 -->
-      <el-sub-menu v-else :index="'/main' + menu.path">
-        <template #title>{{ menu.name }}</template>
-        <el-menu-item
-          v-for="_menu in menu.children"
-          :key="_menu.id"
-          :index="'/main' + _menu.path"
-          >{{ _menu.name }}</el-menu-item
-        >
-      </el-sub-menu>
-    </template>
-  </el-menu>
+<template>
+  <div class="main-header">
+    <h1>项目标题</h1>
+    <!-- 可以在这里添加新业务的导航菜单 -->
+  </div>
 </template>
+
+<style scoped>
+.main-header {
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  height: 100%;
+  background-color: #f5f5f5;
+}
+
+.main-header h1 {
+  margin: 0;
+  color: #333;
+  font-size: 20px;
+}
+</style>
